e8c3b63416ac3e8f7cec62f0df551db97041ad0d
Thomas Steinbach <thomas.steinbach@aikq.de>
2017-12-20 23:13:21 +0100

    allow the non-root stuff and run nextcloud as root in openshift

--- nextcloud/start-docker.sh
+++ nextcloud/start-docker.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-set -ex
-
-# setup new run user
-if ! whoami &> /dev/null; then
-  if [ -w /etc/passwd ]; then
-    echo "${USER_NAME:-default}:x:$(id -u):0:${USER_NAME:-default} user:${HOME}:/sbin/nologin" >> /etc/passwd
-  fi
-fi
-
-APACHE_RUN_USER=www-data
-APACHE_RUN_GROUP=root
-
-/postConfiguration.sh &
+sudo -u www-data --preserve-env /postConfiguration.sh &
 
 exec apache2-foreground
