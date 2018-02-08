59166936fcf9a8d8a05f2b22d704c2ba2bc79958
Thomas Steinbach <thomas.steinbach@aikq.de>
2018-02-08 21:54:52 +0100

    proper sudo oc login as system:admin in setup script

--- setup-project.sh
+++ setup-project.sh
@@ -1,5 +1,5 @@
 oc new-project nextcloud
-sudo oc login
+sudo oc login -u system:admin
 sudo oc adm policy add-scc-to-user anyuid -z default -n nextcloud
 oc new-app -f secret-template.yaml
 oc-apply
