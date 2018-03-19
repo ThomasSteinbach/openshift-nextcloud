a4560d4b567d0d55f60437baf16de74a72985703
Thomas Steinbach <thomas.steinbach@aikq.de>
2018-03-19 12:43:21 +0100

    sleep before build

--- setup-project.sh
+++ setup-project.sh
@@ -3,4 +3,5 @@ sudo oc login -u system:admin
 sudo oc adm policy add-scc-to-user anyuid -z default -n nextcloud
 oc new-app -f secret-template.yaml
 oc-apply
+sleep 10
 oc start-build nextcloud --from-dir .
