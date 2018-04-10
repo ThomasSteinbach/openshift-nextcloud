35f7e96a87b20265bd532c34b800acc89b604b5f
Thomas Steinbach <thomas.steinbach@aikq.de>
2018-04-10 14:53:45 +0200

    use git repo for build source

--- setup-project.sh
+++ setup-project.sh
@@ -4,4 +4,4 @@ sudo oc adm policy add-scc-to-user anyuid -z default -n nextcloud
 oc new-app -f secret-template.yaml
 oc-apply
 sleep 10
-oc start-build nextcloud --from-dir .
+oc start-build nextcloud
