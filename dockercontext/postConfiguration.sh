d60f9874e3f90e7f1cda5a1e405cad03efb029aa
Thomas Steinbach <thomas.steinbach@aikq.de>
2018-04-23 16:15:56 +0200

    do not wait for updates in post install script

--- dockercontext/postConfiguration.sh
+++ dockercontext/postConfiguration.sh
@@ -5,7 +5,7 @@ set -ex
 # wait for nextcloud become installed
 while true; do
   if $(php occ config:system:get installed &> /dev/null); then
-    if [ "$(php occ config:system:get installed)" == 'true' ]; then
+    if [ "$(php occ config:system:get installed)" == 'true' ] || [ "$(php occ config:system:get installed)" = *"require upgrade"* ]; then
       break;
     fi
   fi
