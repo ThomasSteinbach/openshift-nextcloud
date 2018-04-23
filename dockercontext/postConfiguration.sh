c83b483f985b7598364cc162d84c1fa6dd584940
Thomas Steinbach <thomas.steinbach@aikq.de>
2018-04-23 17:19:13 +0200

    explicitly set data directory

--- dockercontext/postConfiguration.sh
+++ dockercontext/postConfiguration.sh
@@ -23,6 +23,7 @@ if [ ! -z $REDIS_HOST ] && [ ! -z $REDIS_PORT ]; then
   php occ config:system:set trusted_domains 2 --value "$(hostname -I | xargs)"
   php occ config:system:set 'overwrite.cli.url' --value "http://$NEXTCLOUD_PUBLIC_HOSTNAME"
   php occ config:system:set 'overwriteprotocol' --value 'https'
+  php occ config:system:set 'datadirectory' --value "$NEXTCLOUD_DATA_DIR"
 
   # must be the last configuration line as Nextcloud will override it
   # after another setting
